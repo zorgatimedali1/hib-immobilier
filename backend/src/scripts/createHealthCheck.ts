2455f36b752ce2387da62d4beeee24a5404a90f6
medalizorgati <medalizorgati8@gmail.com>
2026-06-05 15:10:49 +0100

    feat: update environment configuration and create .gitignore file

--- backend/src/scripts/createHealthCheck.ts
+++ backend/src/scripts/createHealthCheck.ts
@@ -1,11 +1,11 @@
 import pg from 'pg';
 
 const client = new pg.Client({
-  host: 'db.qcozyjrtrickllaxbbxu.supabase.co',
-  port: 5432,
-  user: 'postgres',
-  password: 'hL14QKoyk8TA95SB',
-  database: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || 'postgres',
   ssl: { rejectUnauthorized: false },
 });
 
